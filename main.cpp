903b85e00563794df2c8041b1be0d571865abedc
Daniel Tsokaktsis <119405843+dtsok@users.noreply.github.com>
2024-01-14 21:22:17 +0200

    Test branch, iplem. with arrays.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
+#include <chrono>
 #include <eigen3/Eigen/Dense>
 #include <iostream>
 #include <memory>
+#include <random>
+using namespace std::chrono;
 
-double ELJ(int N, const Eigen::MatrixXd &X, double epsilon = 1, double sigma = 1)
+/*double ELJ(int N, const Eigen::MatrixXd &X, double epsilon = 1, double sigma = 1)
 {
 	double sum = 0;
 	for (size_t i = 0; i < N - 1; i++) {
@@ -27,11 +30,25 @@ inline void swap(Eigen::MatrixXd *&T, const int i, const int j)
 	T[j] = temp;
 }
 
-inline void swap(double *&T, const int i, const int j)
+std::unique_ptr<Eigen::MatrixXd> centerMass(int N, Eigen::MatrixXd *&points)
 {
-	double temp = T[i];
-	T[i] = T[j];
-	T[j] = temp;
+	std::unique_ptr<Eigen::MatrixXd> cm(new Eigen::MatrixXd(N, 3));
+	for (int row = 0; row < N; row++) {
+		for (int col = 0; col < 3; col++) {
+			(*cm)(row, col) = 0;
+		}
+	}
+
+	for (int q = 0; q < N; q++) {
+
+		for (int row = 0; row < N; row++) {
+			for (int col = 0; col < 3; col++) {
+				(*cm)(row, col) += points[q](row, col);
+			}
+		}
+	}
+	(*cm) = (*cm) / N;
+	return cm;
 }
 
 void initialization(int N, Eigen::MatrixXd *&points, double *values)
@@ -76,30 +93,10 @@ void initialization(int N, Eigen::MatrixXd *&points, double *values)
 	// delete[] values;
 }
 
-std::unique_ptr<Eigen::MatrixXd> centerMass(int N, Eigen::MatrixXd *&points)
-{
-	std::unique_ptr<Eigen::MatrixXd> cm(new Eigen::MatrixXd(N, 3));
-	for (int row = 0; row < N; row++) {
-		for (int col = 0; col < 3; col++) {
-			(*cm)(row, col) = 0;
-		}
-	}
-
-	for (int q = 0; q < N; q++) {
-
-		for (int row = 0; row < N; row++) {
-			for (int col = 0; col < 3; col++) {
-				(*cm)(row, col) += points[q](row, col);
-			}
-		}
-	}
-	(*cm) = (*cm) / N;
-	return cm;
-}
-
 int partition(Eigen::MatrixXd *&points, double *a, int l, int h)
 {
-	int R = rand() % (h - l + 1) + l;
+	// int R = rand() % (h - l + 1) + l;
+	int R = h;
 	swap(a, h, R);
 	swap(points, h, R);
 	int pivot = a[h];
@@ -202,13 +199,210 @@ void NelderMead(int N, Eigen::MatrixXd *&points)
 		else {
 			shrinkSimplex(N, points, values);
 		}
-
+		quicksort(points, values, 0, N);
 		of_value = values[0];
 		std::cout << iterations << ": " << of_value << "\n";
 		iterations++;
 	}
 
 	delete[] values;
+}
+*/
+
+/* With arrays */
+
+double l2_norm(int N, const double *X, const int i, const int j)
+{
+	if (std::abs(i - j) < 3) {
+		std::cout << "Error @ l2_norm\n";
+		exit(-1);
+	}
+	double val = 0;
+	for (int z = 0; z < 3; z++) {
+		val += (X[i + z] - X[j + z]) * (X[i + z] - X[j + z]);
+	}
+
+	return std::sqrt(val);
+}
+
+double ELJ(int N, const double *X, double epsilon = 1, double sigma = 1)
+{
+	double sum = 0;
+	for (size_t i = 0; i < 3 * N - 3; i += 3) {
+		double temp = 0;
+		for (size_t j = i + 3; j < 3 * N; j += 3) {
+			double rij = l2_norm(N, X, i, j);
+			double factor = std::pow((sigma / rij), 6);
+			temp += factor * factor - factor;
+		}
+		sum += temp;
+	}
+
+	return 4 * epsilon * sum;
+}
+
+inline void swap(double *T, const int i, const int j)
+{
+	double temp = T[i];
+	T[i] = T[j];
+	T[j] = temp;
+}
+
+inline void swap(double **T, const int i, const int j)
+{
+	double *temp = T[i];
+	T[i] = T[j];
+	T[j] = temp;
+}
+
+void centerMass(int N, double **points, double *cm)
+{
+	for (size_t i = 0; i < 3 * N; i++) {
+		cm[i] = 0;
+	}
+	for (size_t i = 0; i < N; i++) {
+		for (size_t j = 0; j < 3 * N; j++) {
+			cm[j] += points[i][j] / N;
+		}
+	}
+}
+
+int partition(double **points, double *values, int l, int h)
+{
+	// int R = rand() % (h - l + 1) + l;
+	int R = h;
+	swap(values, h, R);
+	swap(points, h, R);
+	int pivot = values[R];
+	int temp_right = l - 1;
+	for (int i = l; i <= h; i++) {
+		if (values[i] < pivot) {
+			temp_right++;
+			swap(values, temp_right, i);
+			swap(points, temp_right, i);
+		}
+	}
+
+	swap(values, temp_right + 1, h);
+	swap(points, temp_right + 1, h);
+
+	return temp_right + 1;
+}
+
+void quicksort(double **points, double *values, int l, int h)
+{
+	if (l < h) {
+		int pivot_part = partition(points, values, l, h);
+
+		// print(a, n);
+		// if (pivot_part > 1) {
+		quicksort(points, values, l, pivot_part - 1);
+		// }
+		// if (pivot_part < h - 1) {
+		quicksort(points, values, pivot_part + 1, h);
+		// }
+	}
+}
+
+void generate_point(double *x_op, double *worst, double *cm, int N, double r_op)
+{
+	/*for (size_t i = 0; i < 3 * N; i++) {
+		x_ref[i] = 0;
+	}*/
+	for (size_t i = 0; i < 3 * N; i++) {
+		x_op[i] = (1 + r_op) * cm[i] - r_op * worst[i];
+	}
+}
+
+void shrinkSimplex(int N, double **points, double *values)
+{
+	for (size_t i = 1; i < N + 1; i++) {
+		for (size_t j = 0; j < 3 * N; j++) {
+			points[i][j] = points[0][j] - (points[i][j] - points[0][j]) * .5;
+		}
+	}
+}
+
+void NelderMead(int N, double **points)
+{
+	double *values = new double[N + 1]();
+	double *cm = new double[3 * N]();
+	double *x_inc = new double[3 * N]();
+	double *x_ref = new double[3 * N]();
+	double *x_exc = new double[3 * N]();
+	double *x_exp = new double[3 * N]();
+
+	for (size_t i = 0; i < N + 1; i++) {
+		values[i] = ELJ(N, points[i]);
+	}
+	std::cout<<"here\n";
+	quicksort(points, values, 0, N);
+
+	const double r_inc = -.5;
+	const double r_exc = .5;
+	const double r_ref = 1;
+	const double r_exp = 2;
+
+	int iterations = 0;
+	int maxIterations = 100000;
+	double real_val = -12.712062;
+	double acc = 0.001;
+	double of_value = values[0];
+	while (of_value > acc + real_val && iterations < maxIterations) {		
+		centerMass(N, points, cm);
+		generate_point(x_ref, points[N], cm, N, r_ref);		
+		double x_ref_val = ELJ(N, x_ref);
+		if (x_ref_val >= values[0] && x_ref_val < values[N - 1]) {
+			points[N] = x_ref;
+			values[N] = x_ref_val;
+		}
+		else if (x_ref_val < values[0]) {
+			// Eigen::MatrixXd x_exp = (1 + r_exp) * (*cm) - r_exp * points[N];
+			generate_point(x_exp, points[N], cm, N, r_exp);
+			double x_exp_val = ELJ(N, x_exp);
+			if (x_exp_val < x_ref_val) {
+				points[N] = x_exp;
+				values[N] = x_exp_val;
+			}
+			else {
+				points[N] = x_ref;
+				values[N] = x_ref_val;
+			}
+		}
+		else if (x_ref_val >= values[N - 1] && x_ref_val < values[N]) {
+			// Eigen::MatrixXd x_exc = (1 + r_exc) * (*cm) - r_exc * points[N];
+			generate_point(x_exc, points[N], cm, N, r_exc);
+			double x_exc_val = ELJ(N, x_exc);
+			if (x_exc_val <= x_ref_val) {
+				points[N] = x_exc;
+				values[N] = x_exc_val;
+			}
+		}
+		else if (values[N] <= x_ref_val) {
+			// Eigen::MatrixXd x_inc = (1 + r_inc) * (*cm) - r_inc * points[N];
+			generate_point(x_inc, points[N], cm, N, r_inc);
+			double x_inc_val = ELJ(N, x_inc);
+			if (x_inc_val < values[N]) {
+				points[N] = x_inc;
+				values[N] = x_inc_val;
+			}
+		}
+		else {
+			shrinkSimplex(N, points, values);
+		}		
+		quicksort(points, values, 0, N);
+		
+		of_value = values[0];
+		std::cout << iterations << ": " << of_value << "\n";
+		iterations++;
+	}
+
+	delete[] values;
+	delete[] cm;
+	delete[] x_inc;
+	delete[] x_ref;
+	delete[] x_exc;
+	delete[] x_exp;
 }
 
 int main(int argc, char const *argv[])
@@ -217,19 +411,56 @@ int main(int argc, char const *argv[])
 	if (argc > 1) {
 		N = std::stoi(argv[1]);
 	}
+	else {
+		return 0;
+	}
+	std::default_random_engine generator;
+	std::uniform_real_distribution<double> dist(-2.5, 2.5);
 
 	// Eigen::MatrixXd cluster = Eigen::MatrixXd(N, 3);
 	// cluster << 0.7430002202, 0.2647603899, -0.0468575389, -0.7430002647, -0.2647604843, 0.0468569750, 0.1977276118, -0.4447220146,
-	// 0.6224700350, -0.1977281310, 0.4447221826, -0.6224697723, -0.1822009635, 0.5970484122, 0.4844363476, 0.1822015272, -0.5970484858,
+	// 0.6224700350, 	-0.1977281310, 0.4447221826, -0.6224697723, -0.1822009635, 0.5970484122, 0.4844363476, 0.1822015272, -0.5970484858,
 	// -0.4844360463;
-	Eigen::MatrixXd *points = new Eigen::MatrixXd[N + 1];
+
+	double **p = new double *[N + 1]; //{0.7430002202, 0.2647603899, -0.0468575389, -0.7430002647, -0.2647604843, 0.0468569750, 0.1977276118,
+									  //-0.4447220146, 0.6224700350,
+									  // -0.1977281310, 0.4447221826, -0.6224697723, -0.1822009635, 0.5970484122, 0.4844363476, 0.1822015272,
+									  // -0.5970484858, -0.4844360463};
+
+	/*Eigen::MatrixXd *points = new Eigen::MatrixXd[N + 1];
 	for (int i = 0; i < N + 1; i++) {
 		points[i] = Eigen::MatrixXd::Random(N, 3);
 	}
+	for (size_t t = 0; t < N + 1; t++) {
+		int count = 0;
+		p[t] = new double[3 * N];
+		for (size_t z = 0; z < N; z++) {
+			for (size_t i = 0; i < 3; i++) {
+				p[t][count++] = points[t](z, i);
+			}
+		}
+	}*/
+	/*auto t1 = high_resolution_clock::now();
+	std::cout << "ELJ with Eigen " << ELJ(N, points[0]) << "\n";
+	auto t2 = high_resolution_clock::now();
+	std::cout << "Time " << duration_cast<milliseconds>(t2 - t1).count() << "\n";
+	t1 = high_resolution_clock::now();
+	std::cout << "ELJ with array " << ELJ(N, p) << "\n";
+	t2 = high_resolution_clock::now();
+	std::cout << "Time " << duration_cast<milliseconds>(t2 - t1).count() << "\n";*/
+	for (size_t t = 0; t < N + 1; t++) {
+		p[t] = new double[3 * N];
+		for (size_t z = 0; z < 3 * N; z++) {			
+			p[t][z] = dist(generator);
+		}
+	}
+	NelderMead(N, p);
 
-	NelderMead(N, points);
-
-	delete[] points;
+	// delete[] points;
+	for (size_t t = 0; t < N + 1; t++) {
+		delete[] p[t];
+	}
+	delete[] p;
 	return 0;
 }
 
